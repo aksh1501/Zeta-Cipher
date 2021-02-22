f7b5e6638d43b4d6351dfd4d1d1a4a0d048ac57c
akshit <akshit.sharma@oracle.com>
2021-02-22 13:07:14 +0530

    recent commit_4

--- src/Constants/Constants.ts
+++ src/Constants/Constants.ts
@@ -1,1 +1,7 @@
 export const aavAlgorithms = ['', 'SPA with CVC2', 'SPA with CVC2(without MerchantName hash)', 'SPA with HMAC', 'SPA with HMAC(without MerchantName hash)']
+
+export const zetaProd = 'zeta'
+
+export const pageInformation = 'Cipher - Authentication Centre '
+
+export const zetaimg = '../assets/zeta.jpeg'
